f7b3927a18d2e8143dbd08c06a148d0cf5f09d43
Nando5P <50nando05@gmail.com>
2025-12-22 11:10:21 +0100

    Actualizacion versiones

--- app/src/androidTest/java/com/example/gestionusuarioshibrido/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/gestionusuarioshibrido/ExampleInstrumentedTest.kt
@@ -17,8 +17,7 @@ import org.junit.Assert.*
 class ExampleInstrumentedTest {
     @Test
     fun useAppContext() {
-        // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.myapplication", appContext.packageName)
+        assertEquals("com.example.gestionusuarioshibrido", appContext.packageName)
     }
 }
